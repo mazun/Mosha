48c351d2dffe3df92343b627c0fc40b7d1bd0d69
y_mazun <y.mazun@gmail.com>
2022-09-23 14:14:37 +0900

    Support paste from clipboard

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ const render = (canvas, image, x, y, size, drawImage) => {
     const height = image.height * (size / 100.0);
 
     canvas.width = image.width;
-    canvas.height = image.height;    
+    canvas.height = image.height;
 
     canvas.style.width = width + 'px';
     canvas.style.height = height + 'px';
 
     ctx.lineWidth = 1 * (100.0 / size);
 
-    if(drawImage) {
+    if (drawImage) {
         ctx.drawImage(image, 0, 0);
     } else {
         ctx.clearRect(0, 0, image.width, image.height);
     }
 
-    for(let i = 0; i < x; i++) {
+    for (let i = 0; i < x; i++) {
         ctx.beginPath();
         ctx.moveTo(image.width * (i + 1) / (x + 1), 0);
         ctx.lineTo(image.width * (i + 1) / (x + 1), image.height);
         ctx.stroke();
     }
 
-    for(let i = 0; i < y; i++) {
+    for (let i = 0; i < y; i++) {
         ctx.beginPath();
         ctx.moveTo(0, image.height * (i + 1) / (y + 1));
         ctx.lineTo(image.width, image.height * (i + 1) / (y + 1));
@@ -64,8 +64,8 @@ const dragover = event => {
 
 const dragend = event => {
     const dt = event.dataTransfer;
-    if(dt.items) {
-        for(let i = 0; i < dt.items.length; i++) {
+    if (dt.items) {
+        for (let i = 0; i < dt.items.length; i++) {
             dt.items.remove(i);
         }
     } else {
@@ -75,21 +75,32 @@ const dragend = event => {
 
 const handleDraggedFile = event => {
     const file = event.dataTransfer.files[0];
+    handleFile(file);
+};
 
+const handleFile = url => {
     const fileReader = new FileReader();
 
-    fileReader.onload = function(event) {
+    fileReader.onload = function (event) {
         const image = new Image();
         image.src = fileReader.result;
-        image.onload = function() {
+        image.onload = function () {
             currentImage = image;
             updateCanvas();
         };
     };
 
-    fileReader.readAsDataURL(file);
+    fileReader.readAsDataURL(url);
     return false;
-};
+}
+
+const handlePaste = pasteEvent => {
+    const item = pasteEvent.clipboardData.items[0];
+
+    if (item.type.indexOf("image") === 0) {
+        handleFile(item.getAsFile());
+    }
+}
 
 const init = () => {
     const canvas = document.getElementById('canvas');
@@ -97,6 +108,7 @@ const init = () => {
     canvas.ondragend = dragend;
     canvas.ondragover = dragover;
     canvas.ondrop = handleDraggedFile;
+    document.onpaste = handlePaste;
 
     updateCanvas();
 }
